5c581af2cb3ddd8f9023064f4f5133d507210f61
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-07-11 21:45:22 +0800

    paldo sana bukas

--- Frontend/queingapp/lib/data/models/Auth/user_dto.dart
+++ Frontend/queingapp/lib/data/models/Auth/user_dto.dart
@@ -1,3 +1,31 @@
 class UserDto {
-  
+  final String? id;
+  final String? name;
+  final String? username;
+  final String? password;
+
+  UserDto({
+    this.id,
+    this.name,
+    this.password,
+    this.username
+  });
+
+  factory UserDto.fromJson(Map<String, dynamic> json){
+    return UserDto(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      username: json['username'] as String,
+      password: json['password'] as String
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'id':id,
+      'name':name,
+      'username':username,
+      'password': password
+    };
+  }
 }
