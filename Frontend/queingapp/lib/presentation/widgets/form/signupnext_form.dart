307ca87b50d3466bbc4776d2efa8f448e651e034
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-07-12 22:38:59 +0800

    done

--- Frontend/queingapp/lib/presentation/widgets/form/signupnext_form.dart
+++ Frontend/queingapp/lib/presentation/widgets/form/signupnext_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:queingapp/presentation/widgets/buttons/reusable_button.dart';
+import 'package:queingapp/presentation/widgets/inputs/reusable_field.dart';
 
 class SignupnextForm extends StatefulWidget {
   const SignupnextForm({super.key});
@@ -8,18 +10,88 @@ class SignupnextForm extends StatefulWidget {
 }
 
 class _SignupnextFormState extends State<SignupnextForm> {
+  final _usernameController = TextEditingController();
+  final _passwordController   = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
   final _key = GlobalKey<FormState>();
+  bool isObscure = true;
   @override
   void dispose() {
-    // TODO: implement dispose
+    _usernameController.dispose();
+    _confirmPasswordController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _key,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-            
+            const SizedBox(
+            height: 30,
+          ),
+          const Text("CREATE USERNAME"),
+          const SizedBox(
+            height: 10,
+          ),
+          ReusableField(controller: _usernameController),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text("CREATE PASSWORD"),
+          const SizedBox(
+            height: 10,
+          ),
+          ReusableField(
+            controller: _passwordController,
+            isObscure: isObscure,
+            suffixIcon: IconButton(
+              onPressed: (){
+                setState(() {
+                  isObscure = !isObscure;
+                });
+              },
+              icon: Icon(
+                isObscure? Icons.visibility_off : Icons.visibility
+              ),
+            ),
+            ),
+            const SizedBox(
+            height: 30,
+          ),
+          const Text("CONFIRM PASSWORD"),
+          const SizedBox(
+            height: 10,
+          ),
+          ReusableField(
+            controller: _confirmPasswordController,
+            isObscure: isObscure,
+            suffixIcon: IconButton(
+              onPressed: (){
+                setState(() {
+                  isObscure = !isObscure;
+                });
+              },
+              icon: Icon(
+                isObscure? Icons.visibility_off : Icons.visibility
+              ),
+            ),
+            ),
+            const SizedBox(
+            height: 120,
+          ),
+          Center(
+            child: ReusableButton(
+            title: "Sign Up", 
+            onPressed: (){}, 
+            width: 200, 
+            height: 50,
+            backgroundColor: Colors.black,
+            textColor: Colors.white,
+          ),
+          )
           ],
         )
       );
